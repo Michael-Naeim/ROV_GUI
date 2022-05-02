8d9adcd1ee12221a958c9cad5e028f2c9630992f
Michael Naeim <66870124+Michael-Naeim@users.noreply.github.com>
2022-05-02 09:56:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,538 @@
+// 1st range
+const slider = document.querySelector(".range_for_slide");
+const value1 = document.querySelector("#value_micro");
+
+let status = 'Stopped';
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours =  0;
+
+let interval = '';
+
+let displaySeconds2 = 0;
+let displayMinutes2 = 0;
+value1.textContent = slider.value;
+slider.oninput = function() {
+    if(value1.textContent < 0){
+        value1.style.color = "Red";
+    }else if (value1.textContent  > 0){
+        value1.style.color = "Green";
+    }
+    value1.textContent = this.value;
+}
+    
+
+// 2nd slider
+const slider2 = document.querySelector(".v2");
+const value2 = document.querySelector(".v2-3");
+
+value2.textContent = slider2.value;
+slider2.oninput = function() {
+    
+    if(value2.textContent<0){
+        value2.textContent = this.value;
+        value2.style.color = 'Red';
+    }else if (value2.textContent>0){
+        value2.textContent = this.value;
+        value2.style.color = 'Green';
+    }else if(value2.textContent == 0){
+        value2.textContent = this.value;
+    }
+}
+
+// 3rd slider in the first column
+const slider3 = document.querySelector("#third_range");
+const value3 = document.querySelector("#third_value");
+
+value3.textContent = slider2.value;
+slider3.oninput = function() {
+    
+    if(value3.textContent<0){
+        value3.textContent = this.value;
+        value3.style.color = 'Red';
+    }else if (value3.textContent>0){
+        value3.textContent = this.value;
+        value3.style.color = 'Green';
+    }else if(value3.textContent == 0){
+        value3.textContent = this.value;
+    }
+}
+
+// 4th row
+const slider4 = document.querySelector("#s3-1");
+const value4 = document.querySelector("#s3-v1");
+
+value4.textContent = slider2.value;
+slider4.oninput = function() {
+    
+    if(value4.textContent<0){
+        value4.textContent = this.value;
+        value4.style.color = 'Red';
+    }else if (value4.textContent>0){
+        value4.textContent = this.value;
+        value4.style.color = 'Green';
+    }else if(value4.textContent == 0){
+        value4.textContent = this.value;
+    }
+}
+
+// 5th item
+const slider5 = document.querySelector("#s3-2");
+const value5 = document.querySelector("#s3-v2");
+
+value5.textContent = slider2.value;
+slider5.oninput = function() {
+    
+    if(value5.textContent<0){
+        value5.textContent = this.value;
+        value5.style.color = 'Red';
+    }else if (value5.textContent>0){
+        value5.textContent = this.value;
+        value5.style.color = 'Green';
+    }else if(value5.textContent == 0){
+        value5.textContent = this.value;
+    }
+}
+
+// 6th item
+const slider6 = document.querySelector("#s3-3");
+const value6 = document.querySelector("#s3-v3");
+
+value6.textContent = slider2.value;
+slider6.oninput = function() {
+    
+    if(value6.textContent<0){
+        value6.textContent = this.value;
+        value6.style.color = 'Red';
+    }else if (value6.textContent>0){
+        value6.textContent = this.value;
+        value6.style.color = 'Green';
+    }else if(value6.textContent == 0){
+        value6.textContent = this.value;
+    }
+}
+
+// 7th item
+const slider7 = document.querySelector("#s3-4");
+const value7 = document.querySelector("#s3-v4");
+
+value7.textContent = slider2.value;
+slider7.oninput = function() {
+    
+    if(value7.textContent<0){
+        value7.textContent = this.value;
+        value7.style.color = 'Red';
+    }else if (value7.textContent>0){
+        value7.textContent = this.value;
+        value7.style.color = 'Green';
+    }else if(value7.textContent == 0){
+        value7.textContent = this.value;
+    }
+}
+
+
+// 8th item
+const slider8 = document.querySelector("#s3-5");
+const value8 = document.querySelector("#s3-v5");
+
+value8.textContent = slider2.value;
+slider8.oninput = function() {
+    
+    if(value8.textContent<0){
+        value8.textContent = this.value;
+        value8.style.color = 'Red';
+    }else if (value8.textContent>0){
+        value8.textContent = this.value;
+        value8.style.color = 'Green';
+    }else if(value8.textContent == 0){
+        value8.textContent = this.value;
+    }
+}
+
+// 9th item
+const slider9 = document.querySelector("#s3-6");
+const value9 = document.querySelector("#s3-v6");
+
+value9.textContent = slider2.value;
+slider9.oninput = function() {
+    
+    if(value9.textContent<0){
+        value9.textContent = this.value;
+        value9.style.color = 'Red';
+    }else if (value9.textContent>0){
+        value9.textContent = this.value;
+        value9.style.color = 'Green';
+    }else if(value9.textContent == 0){
+        value9.textContent = this.value;
+    }
+}
+
+// add and subtract
+
+    var box = document.getElementById('numy')
+    var next = document.querySelector('.next')
+    var prev = document.querySelector('.prev')
+
+    box.textContent = 0;
+    next.onclick = function(){
+        box.textContent++;
+        box.style.color = "#c24d2c";
+    }
+    prev.onclick = function(){
+        box.textContent--;
+    }
+
+//second element
+
+    var box2 = document.getElementById('numy2')
+    var next2 = document.querySelector('.next-1')
+    var prev2= document.querySelector('.prev-1')
+
+    box2.textContent = 0;
+    next2.onclick = function(){
+        box2.textContent++;
+        box2.style.color = "#c24d2c";
+    }
+    prev2.onclick = function(){
+        box2.textContent--;
+    }
+
+
+// count down time
+
+const slider10 = document.querySelector("#s4-1");
+const value10 = document.querySelector("#s4-v1");
+
+value10.textContent = slider10.value;
+slider10.oninput = function() {
+    
+    if(value10.textContent<0){
+        value10.textContent = this.value;
+        value10.style.color = 'Red';
+    }else if (value10.textContent>0){
+        value10.textContent = this.value;
+        value10.style.color = 'Green';
+    }else if(value10.textContent == 0){
+        value10.textContent = this.value;
+    }
+}
+
+const slider11 = document.querySelector("#s4-2");
+const value11 = document.querySelector("#s4-v2");
+
+value11.textContent = slider11.value;
+slider11.oninput = function() {
+    
+    if(value11.textContent<0){
+        value11.textContent = this.value;
+        value11.style.color = 'Red';
+    }else if (value11.textContent>0){
+        value11.textContent = this.value;
+        value11.style.color = 'Green';
+    }else if(value11.textContent == 0){
+        value11.textContent = this.value;
+    }
+}
+
+
+// identify first-second and third show to show item when click a button (related to controls including PID Factors and that stuff)
+const PID_FACTORS = document.getElementById("PID_FACTORS");
+const Buttons = document.getElementById("Buttons");
+const Axis = document.getElementById("Axis");
+
+// items that we will make it appear when clicking on a specific buttons
+PID_FACTORS.addEventListener('click', (e)=>{
+    // show only first
+    document.getElementById("first_show").classList.toggle("show");
+
+    document.getElementById("first_show").classList.toggle("hide");
+
+    document.getElementById("second_show").classList.remove("show");
+    document.getElementById("second_show").classList.add("hide");
+
+    document.getElementById("third_show").classList.remove("show");
+    document.getElementById("third_show").classList.add("hide");
+
+    if(document.getElementById("first_show").classList.contains("show")){
+        document.querySelector(".blur_help").classList.add("blur")
+    }else{
+        document.querySelector(".blur_help").classList.remove("blur")
+    }
+
+
+
+})
+
+Buttons.addEventListener('click', (e)=>{
+    // show only second
+    document.getElementById("first_show").classList.remove("show");
+    document.getElementById("first_show").classList.add("hide");
+
+    document.getElementById("second_show").classList.toggle("show");
+    document.getElementById("second_show").classList.toggle("hide");
+
+    
+    document.getElementById("third_show").classList.remove("show");
+    document.getElementById("third_show").classList.add("hide");
+
+    if(document.getElementById("second_show").classList.contains("show")){
+        document.querySelector(".blur_help").classList.add("blur")
+    }else{
+        document.querySelector(".blur_help").classList.remove("blur")
+    }
+    
+})
+
+Axis.addEventListener('click', (e)=>{
+    // show only third
+    document.getElementById("first_show").classList.remove("show");
+    document.getElementById("first_show").classList.add("hide");
+
+    document.getElementById("second_show").classList.remove("show");
+    document.getElementById("second_show").classList.add("hide");
+
+    document.getElementById("third_show").classList.toggle("show");
+    document.getElementById("third_show").classList.toggle("hide");
+
+    if(document.getElementById("third_show").classList.contains("show")){
+        document.querySelector(".blur_help").classList.add("blur")
+    }else{
+        document.querySelector(".blur_help").classList.remove("blur")
+    }
+})
+
+// add and subtract in the PID FACTOR
+var el1_ne0 = document.querySelector('.el1_ne0')
+var el1_ne1 = document.querySelector('.el1_ne1')
+var el1_ne2 = document.getElementById('el1_ne2')
+
+el1_ne2.textContent = 0;
+el1_ne0.onclick = function(){
+    el1_ne2.textContent++;
+    el1_ne2.style.color = "#c24d2c";
+}
+el1_ne1.onclick = function(){
+    el1_ne2.textContent--;
+}
+
+// second element
+var el2_ne0 = document.querySelector('.el2_ne0')
+var el2_ne1 = document.querySelector('.el2_ne1')
+var el2_ne2 = document.getElementById('el2_ne2')
+
+el2_ne2.textContent = 0;
+el2_ne0.onclick = function(){
+    el2_ne2.textContent++;
+    el2_ne2.style.color = "#c24d2c";
+}
+el2_ne1.onclick = function(){
+    el2_ne2.textContent--;
+}
+//third element
+var el3_ne0 = document.querySelector('.el3_ne0')
+var el3_ne1 = document.querySelector('.el3_ne1')
+var el3_ne2 = document.getElementById('el3_ne2')
+
+el3_ne3.textContent = 0;
+el3_ne0.onclick = function(){
+    el3_ne3.textContent++;
+    el3_ne3.style.color = "#c24d2c";
+}
+el3_ne1.onclick = function(){
+    el3_ne3.textContent--;
+}
+
+// timer
+
+
+function StartWatch()
+{
+	seconds++;
+
+	if(seconds/60 === 1)
+	{
+		seconds=0;
+		minutes++;
+
+		if(minutes/60 === 1)
+		{
+			minutes=0;
+			hours++;
+		}
+	}
+
+	if(seconds < 10 )
+	{
+		displaySeconds = '0' + seconds.toString();
+	}
+	else
+	{
+		displaySeconds = seconds;
+	}
+
+	if(minutes < 10 )
+	{
+		displayMinutes = '0' + minutes.toString();
+	}
+	else
+	{
+		displayMinutes = minutes;
+	}
+
+	if(hours < 10 )
+	{
+		displayHours = '0' + hours.toString();
+	}
+	else
+	{
+		displayHours = hours;
+	}
+
+	document.getElementById('display').innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+}
+
+function startStop()
+{
+	if(status == 'Stopped')
+	{
+		interval = window.setInterval(StartWatch,1000);
+		document.getElementById('handler').innerHTML = 'Stop';
+		status = 'Started';
+	} 
+	else if(status == 'Started')
+	{
+		window.clearInterval(interval);
+		document.getElementById('handler').innerHTML = 'Start';
+		status = 'Stopped';
+	}
+}
+
+function Reset()
+{
+	seconds = 0;
+	hours = 0;
+	minutes = 0;
+	window.clearInterval(interval);
+	document.getElementById('display').innerHTML = '00:00:00';
+	document.getElementById('handler').innerHTML = 'Start';
+	status = 'Stopped';
+}
+
+// stopwatch - countdown colors
+const stopwatch = document.getElementById("stopwatch");
+stopwatch.addEventListener('click', ()=>{
+    document.getElementById("stopwatch_1").classList.add("show");
+    document.getElementById("stopwatch_2").classList.add("show");
+    document.getElementById("stopwatch_3").classList.add("show");
+   
+    document.getElementById("stopwatch_4").style.display="flex !important";
+    // remove hide
+    document.getElementById("stopwatch_1").classList.remove("hide");
+    document.getElementById("stopwatch_2").classList.remove("hide");
+    document.getElementById("stopwatch_3").classList.remove("hide");
+    document.getElementById("stopwatch_4").classList.remove("hide");
+    document.querySelector(".StopwatchORCountdown").classList.add("hide");
+
+})
+
+
+// Count_Down
+const start_btn = document.getElementById("handler_2");
+
+document.querySelector(".range_1").addEventListener("change",()=>{
+    if(document.querySelector(".range_1_value").textContent < 10 )
+	{
+		displayMinutes2 = '0' + document.querySelector(".range_1_value").textContent;
+	}
+	else
+	{
+		displayMinutes2 = document.querySelector(".range_1_value").textContent;
+	}
+    document.getElementById('no_1').textContent =  displayMinutes2;
+})
+
+document.querySelector(".range_2").addEventListener("change",()=>{
+    if(document.querySelector(".range_2_value").textContent < 10 )
+	{
+		displaySeconds2 = '0' + document.querySelector(".range_2_value").textContent;
+	}
+	else
+	{
+		displaySeconds2 = document.querySelector(".range_2_value").textContent;
+	}
+    
+    document.getElementById('no_2').textContent =  displaySeconds2;
+})
+
+
+// start
+start_btn.addEventListener('click', (e)=>{
+    var time = setInterval(function (){timer()}, 1000);
+    document.getElementById("reset_2").addEventListener("click", ()=>{
+        clearInterval(time)
+        document.getElementById('no_1').textContent =  00;
+        document.getElementById('no_2').textContent =  00;
+        
+    })
+})
+function timer(){
+    displaySeconds2= displaySeconds2 -1;
+
+    if(displaySeconds2 == -1){
+        displayMinutes2--;
+        displaySeconds2= 59;
+    }
+
+    if(displayMinutes2 == -1){
+        displaySeconds2 =0;
+        displayMinutes2= 0;
+    }
+    document.getElementById('no_1').textContent =  displayMinutes2;
+    document.getElementById('no_2').textContent =  displaySeconds2;
+    
+}
+
+// CountDown show
+document.getElementById("Countdown").addEventListener("click", ()=>{
+    document.querySelector(".countdown").classList.add("show");
+    document.querySelector(".countdown").classList.remove("hide");
+    document.querySelector(".StopwatchORCountdown").classList.add("hide");
+    document.querySelector(".StopwatchORCountdown").classList.remove("show");
+})
+
+// switch to stopwatch
+document.getElementById("stopwatch_switch").addEventListener("click",()=>{
+    document.querySelector(".countdown").classList.remove("show");
+    document.querySelector(".countdown").classList.add("hide");
+    // show actual stopwatch
+    document.getElementById("stopwatch_1").classList.add("show");
+    document.getElementById("stopwatch_2").classList.add("show");
+    document.getElementById("stopwatch_3").classList.add("show");
+   
+    document.getElementById("stopwatch_4").style.display="flex !important";
+    // remove hide
+    document.getElementById("stopwatch_1").classList.remove("hide");
+    document.getElementById("stopwatch_2").classList.remove("hide");
+    document.getElementById("stopwatch_3").classList.remove("hide");
+    document.getElementById("stopwatch_4").classList.remove("hide");
+})
+
+
+document.getElementById("countDown_switch").addEventListener("click",()=>{
+    document.querySelector(".countdown").classList.add("show");
+    document.querySelector(".countdown").classList.remove("hide");
+    // show actual stopwatch
+    document.getElementById("stopwatch_1").classList.remove("show");
+    document.getElementById("stopwatch_2").classList.remove("show");
+    document.getElementById("stopwatch_3").classList.remove("show");
+   
+    // remove hide
+    document.getElementById("stopwatch_1").classList.add("hide");
+    document.getElementById("stopwatch_2").classList.add("hide");
+    document.getElementById("stopwatch_3").classList.add("hide");
+    document.getElementById("stopwatch_4").classList.add("hide");
+})
---
+++ main.js
@@ -0,0 +1,65 @@
+const {app, BrowserWindow, Menu, globalShortcut } = require('electron')
+const path = require('path')
+const url = require('url')
+const contextMenu = require('electron-context-menu');
+contextMenu({
+	showSaveImageAs:true,
+});
+
+// setting up the environment as development
+
+const isDev = process.env.NODE_ENV !== 'production' ? false : true;
+let mainWindow  
+
+// size, icons and some specification for the app
+function createWindow () {
+   mainWindow = new BrowserWindow({
+    // width: 850,
+    // height: 640,
+    resizable : false,
+    icon: __dirname + '/pictures/logo.png',
+    webPreferences: {
+      nodeIntegration : true,spellcheck: true,
+    },
+  })
+  mainWindow.maximize()
+
+  // load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }))
+
+}
+// when the app opens
+app.on('ready', ()=>{
+  createWindow()
+// some shortcuts
+  globalShortcut.register('ctrl + R', ()=>mainWindow.reload())
+  globalShortcut.register('ctrl + Shift + I', ()=>mainWindow.toggleDevTools())
+  globalShortcut.register('ctrl + Q ', ()=>mainWindow.close())
+  globalShortcut.register('ctrl + M', ()=>mainWindow.minimize())
+
+  // hide default file menu
+  mainWindow.setMenuBarVisibility(false)
+
+  mainWindow.on('ready', ()=> mainWindow = null)
+})
+
+
+// Quit when all windows are closed.
+
+app.on('window-all-closed', () => {
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app
+  if (win === null) {
+    createWindow()
+  }
+})
